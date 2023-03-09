71cb22f1b825a365361a279bc104aa5dda9183f9
Cesar Li Chen <cesarli619@gmail.com>
2023-03-08 22:27:42 -0500

    Moves form from html to js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 console.log("app")
 
 const formView = new FormView();
-
 formView.render();
 console.log(formView.el);
+$("body").append(formView.el);
 
 // $ = function
 // ("[css selector]")
 // $("h1").hide();
-
-$("body").append(formView.el);
--- js/views/form.js
+++ js/views/form.js
@@ -1,9 +1,37 @@
 console.log("form")
 
 const FormView = Backbone.View.extend({
+    tagName: "form",
+    id: "new-expense-form",
+
     render() {
         // By default Backbone's elements are <div>
-        this.el.innerHTML = "Hello";
+        this.el.innerHTML = this.markup;
         return this;
-    }
-})
+    },
+
+    markup: `
+        <div class="row">
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input id="description" name="description" type="text" placeholder="Electronics">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group">
+                <label for="date">Date</label>
+                <input id="date" name="date" type="text" placeholder="yyyy-mm-dd">
+            </div>
+            <div class="form-group">
+                <label for="amount">Amount</label>
+                <input id="amount" name="amount" type="text" placeholder="29.98">
+            </div>
+        </div>
+
+        <div class="form-actions">
+            <button type="reset">Cancel</button>
+            <button type="submit">Save</button>
+        </div>
+    `,
+});
