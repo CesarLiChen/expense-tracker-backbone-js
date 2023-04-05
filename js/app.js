91582e209082b22615ae5a3ba678139b7e0a7b9d
Cesar Li Chen <cesarli619@gmail.com>
2023-04-04 22:05:10 -0400

    Creates the expenses view, output collection length

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ console.log("app")
 
 const collection = new Expenses();
 const formView = new FormView({ collection });
+const expensesView = new ExpensesView({ collection });
+
 formView.render();
+expensesView.render();
 console.log(formView.el);
+
 $("body").append(formView.el);
+$("body").append(expensesView.el);
 
 // $ = function
 // ("[css selector]")
---
+++ js/views/expenses.js
@@ -0,0 +1,15 @@
+// Displays the collection
+
+const ExpensesView = Backbone.View.extend({
+    tagName: "div",
+    id: "expenses",
+
+    initialize() {
+        this.listenTo(this.collection, "add", this.render);
+    },
+
+    render() {
+        this.el.innerHTML = this.collection.length;
+        return this;
+    }
+});
