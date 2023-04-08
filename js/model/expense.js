8bfa1c4f42ea8a177e801017386394cb6edbcb34
Cesar Li Chen <cesarli619@gmail.com>
2023-04-07 22:41:31 -0400

    Creates ID generating function

--- js/model/expense.js
+++ js/model/expense.js
@@ -2,7 +2,12 @@ const Expense = Backbone.Model.extend({
 
     // initialize method only tells us when something is created.
     
-    // initialize() {
-    //     console.log("I'm a new expense")
-    // }
+    initialize() {
+        console.log("I'm a new expense")
+        this.set("id", this.generateId())
+    },
+    
+    generateId() {
+        return new Date().getTime();
+    }
 });
