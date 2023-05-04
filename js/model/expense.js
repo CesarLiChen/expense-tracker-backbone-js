210e1569de282f3fb920adb9dbfd6fea28e59aa1
Cesar Li Chen <cesarli619@gmail.com>
2023-05-04 16:44:19 -0400

    Adds regex to validate date

--- js/model/expense.js
+++ js/model/expense.js
@@ -17,13 +17,20 @@ const Expense = Backbone.Model.extend({
     validate(expense) { // Special method in Backbone
         console.log("validate: ")
         console.log(expense);
-        return this.validateAmount(expense.amount);
+        return this.validateAmount(expense.amount) ||
+            this.validateDate(expense.date);
        
     },
 
     validateAmount(amount) {
         if (amount.length === 0 || !amount.match(/^\$?(\d*)(\.\d{0,2})?$/)) {
             return "Invalid amount";
+        }
+    },
+
+    validateDate(date) {
+        if (!date.match(/^\d{4}-\d{1,2}-\d{1,2}$/) || isNaN(Date.parse(date))) {
+            return "Invalid date";
         }
     }
 });
