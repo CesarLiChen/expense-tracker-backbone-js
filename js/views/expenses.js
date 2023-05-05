6ef04c487783611fb6eb02b2704e87917c614114
Cesar Li Chen <cesarli619@gmail.com>
2023-05-04 21:44:37 -0400

    Creates sorting visuals

--- js/views/expenses.js
+++ js/views/expenses.js
@@ -4,12 +4,34 @@ const ExpensesView = Backbone.View.extend({
     tagName: "div",
     id: "expenses",
 
+    ASC_SYMBOL: "&#9660;", 
+    DESC_SYMBOL: "&#9650;",
+
+    template: _.template(`
+        <div id="sort-actions">
+            Sort:
+            <a href="#/sort/date/<%= dateDirection %>" id="sort-date">
+                Date <%= dateSymbol %>
+            </a>
+            <a href="#/sort/amount/<%= amountDirection %>" id="sort-amount">
+                Amount <%= amountSymbol %>
+            </a>
+        </div>
+    `),
+
     initialize() {
         this.listenTo(this.collection, "add", this.render);
     },
 
     render() {
-        this.el.innerHTML = ""; //Clears view before adding expenses
+        // this.el.innerHTML = ""; //Clears view before adding expenses
+        this.el.innerHTML = this.template({
+            dateDirection:  "asc",
+            amountDirection: "asc",
+
+            dateSymbol: "",
+            amountSymbol: this.ASC_SYMBOL,
+        });
 
         this.collection.forEach( (expense) => {
             this.addExpense(expense);
